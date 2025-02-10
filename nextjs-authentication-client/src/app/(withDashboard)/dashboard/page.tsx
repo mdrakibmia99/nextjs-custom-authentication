ea6901ea11c922e2b8e6c9fc8eb7a355a95b89a0
mdrakibmia99 <rkrakibhasan680@gmail.com>
2025-02-10 15:37:23 +0600

    login with github done

--- nextjs-authentication-client/src/app/(withDashboard)/dashboard/page.tsx
+++ nextjs-authentication-client/src/app/(withDashboard)/dashboard/page.tsx
@@ -1,7 +1,28 @@
-const DashboardPage = () => {
+import { authOptions } from "@/utils/authOptions";
+import { getServerSession } from "next-auth";
+import Image from "next/image";
+
+const DashboardPage = async () => {
+  const session = await getServerSession(authOptions);
+  console.log(session);
+
   return (
     <div>
-      <h1 className="text-4xl text-center mt-10">Welcome To Dashboard Page</h1>
+      {session?.user && (
+        <>
+          <h1 className="text-4xl text-center mt-10">
+            Welcome {session?.user?.name}
+          </h1>
+          <p className="text-4xl text-center my-10">You are logged in as {session?.user?.email}</p>
+          <Image
+            alt="profileImage"
+            width={100}
+            height={100}
+            src={session?.user?.image as string}
+            className="mx-auto rounded-full"
+          />
+        </>
+      )}
     </div>
   );
 };
