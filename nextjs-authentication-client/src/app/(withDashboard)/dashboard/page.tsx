b60b00bb266754e12eb7a103e6ee769aca959897
mdrakibmia99 <rkrakibhasan680@gmal.com>
2025-02-12 00:59:02 +0600

    update

--- nextjs-authentication-client/src/app/(withDashboard)/dashboard/page.tsx
+++ nextjs-authentication-client/src/app/(withDashboard)/dashboard/page.tsx
@@ -1,11 +1,11 @@
+//md rakib mia 
+// mern stact developer
 import { authOptions } from "@/utils/authOptions";
 import { getServerSession } from "next-auth";
 import Image from "next/image";
 
 const DashboardPage = async () => {
   const session = await getServerSession(authOptions);
-  console.log(session);
-
   return (
     <div>
       {session?.user && (
